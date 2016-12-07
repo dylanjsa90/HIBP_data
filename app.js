e0fe336a7b52305b84eb0280adddb26c3fc2568a
dylanjsa90 <dylanjsanders1@gmail.com>
2016-12-06 16:07:43 -0800

    have initial data reading functionality finished, removed the data from git due to size

---
+++ app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const fs = require('fs');
+const readStream = fs.createReadStream('data/HIBP_data.txt');
+let fileRead;
+let obj = {};
+
+readStream.on('readable', () => {
+  let chunk;
+  while(null !== (chunk = readStream.read())) {
+    fileRead = (fileRead === undefined) ? fileRead = chunk : fileRead += chunk;
+  }
+});
+
+readStream.on('end', () => {
+  console.log('Typeof fileRead: ' + typeof fileRead);
+  console.log('Read Stream Complete');
+
+  let splitLines = fileRead.split('\r\n');
+  parseData(splitLines);
+  console.log('Object: ', obj);
+});
+
+function parseData(inputLine) {
+  inputLine.forEach((e) => {
+    let parsed = e.split(' ');
+    let num = parseInt(parsed[1]);
+    let emails = parsed[0].split(';').slice(1);
+    for (var i = 0; i < emails.length; i++) {
+      if (!obj.hasOwnProperty(emails[i])) {
+        obj[emails[i]] = {};
+        obj[emails[i]].count = 0;
+        obj[emails[i]].related = [];
+      }
+      obj[emails[i]].count += num;
+      let relatedArray = emails.filter((email) => {
+        return email !== emails[i];
+      });
+      
+      obj[emails[i]].related.push(relatedArray.join(' '));
+    }
+  });
+}
---
+++ data/readme.txt
@@ -0,0 +1,9 @@
+The background and purpose of the data is documented in this blog post: https://www.troyhunt.com/heres-1-4-billion-records-from-have-i-been-pwned-for-you-to-analyse
+
+Each row contains a unique set of pwned websites delimited by a semicolon and followed by a count indicating how many unique email addresses these sites appear against. For example:
+
+Adobe;Aipai;CivilOnline;NetEase;NexusMods 20
+
+There are 20 unique email addresses that appear in this exact combination of 5 data breaches.
+
+The data is an aggregate of 1,431,112,732 records being the total number of email addresses in HIBP as of 6 December 2016, EXCLUDING those that only appear in a "sensitive" data breach. Full details in the blog post above.
