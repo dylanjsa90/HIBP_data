f068c61d634b7c3a7296a0f8dddb1177c037cf38
dylanjsa90 <dylanjsanders1@gmail.com>
2016-12-06 17:28:20 -0800

    added module to return an array of pulled api data

--- app_api.js
+++ app_api.js
@@ -1,14 +1,15 @@
 'use strict';
 
 const request = require('request');
-const Company = require('./model/Company');
-
-
-module.exports = apiData = function() {
+let e = {};
+e.data = [];
+e.fetchAPIData = function() {
   request('https://haveibeenpwned.com/api/v2/breaches', (err, response, body) => {
     if (err || response.statusCode !== 200) console.log(err);
     response.forEach((i) => {
-      Company(i).save()
-    })
-  })
-} 
+      e.data.push(i);      
+    });
+  });
+};
+
+module.exports = exports = e;
